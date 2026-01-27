87735f1921503f7256707916a8c2f46ff6fe5e32
Bruno Lopes <bruno.guitarpro@gmail.com>
2026-01-26 22:34:44 -0300

    Fix UNUserNotificationCenterDelegate conformance warning

--- Pulse/PulseAppDelegate.swift
+++ Pulse/PulseAppDelegate.swift
@@ -99,14 +99,14 @@ final class PulseAppDelegate: UIResponder, UIApplicationDelegate {
 }
 
 extension PulseAppDelegate: UNUserNotificationCenterDelegate {
-    nonisolated func userNotificationCenter(
+    @MainActor func userNotificationCenter(
         _: UNUserNotificationCenter,
         willPresent _: UNNotification
     ) async -> UNNotificationPresentationOptions {
         [.banner, .sound, .badge]
     }
 
-    nonisolated func userNotificationCenter(
+    @MainActor func userNotificationCenter(
         _: UNUserNotificationCenter,
         didReceive response: UNNotificationResponse
     ) async {
